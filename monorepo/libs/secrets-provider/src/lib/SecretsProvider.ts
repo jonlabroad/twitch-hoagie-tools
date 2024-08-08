30957de60c2247c1e933349510738ba9a9ecbb53
Jon LaBroad <jon.labroad@gmail.com>
2024-08-07 17:58:36 -0700

    prevent uninitialized secrets provider usage

--- monorepo/libs/secrets-provider/src/lib/SecretsProvider.ts
+++ monorepo/libs/secrets-provider/src/lib/SecretsProvider.ts
@@ -1,37 +1,51 @@
-import { DynamoDBClient, QueryCommand, QueryCommandInput } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import {
+  DynamoDBClient,
+  QueryCommand,
+  QueryCommandInput,
+} from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export class SecretsProvider {
-    private static instance: SecretsProvider;
-    public secrets: Record<string, any> = {};
+  private static instance: SecretsProvider;
+  public secrets: Record<string, any> = {};
+  public static initialized = false;
 
-    private constructor() { }
+  private constructor() {}
 
-    public static async init() {
-        const instance = SecretsProvider.getInstance();
-        const dbClient = new DynamoDBClient({ region: "us-east-1" });
-        const docClient = DynamoDBDocumentClient.from(dbClient);
+  public static async init() {
+    const instance = SecretsProvider.getInstance();
+    const dbClient = new DynamoDBClient({ region: 'us-east-1' });
+    const docClient = DynamoDBDocumentClient.from(dbClient);
 
-        const response = await docClient.send(new QueryCommand({
-            TableName: "HoagieTools-prod",
-            KeyConditionExpression: `#CategoryKey = :catkey`,
-            ExpressionAttributeNames: {
-                "#CategoryKey": `CategoryKey`
-            },
-            ExpressionAttributeValues: {
-                ":catkey": { "S": "SECRETS" }
-            }
-        } as QueryCommandInput));
-        response.Items?.forEach(item => {
-            instance.secrets[item?.["SubKey"].S ?? ""] = item["value"].S;
-        })
+    const response = await docClient.send(
+      new QueryCommand({
+        TableName: 'HoagieTools-prod',
+        KeyConditionExpression: `#CategoryKey = :catkey`,
+        ExpressionAttributeNames: {
+          '#CategoryKey': `CategoryKey`,
+        },
+        ExpressionAttributeValues: {
+          ':catkey': { S: 'SECRETS' },
+        },
+      } as QueryCommandInput)
+    );
+    response.Items?.forEach((item) => {
+      instance.secrets[item?.['SubKey'].S ?? ''] = item['value'].S;
+    });
+    SecretsProvider.initialized = true;
+  }
+
+  public static getInstance(): SecretsProvider {
+    if (!SecretsProvider.initialized) {
+      throw new Error(
+        'SecretsProvider not initialized. Call SecretsProvider.init() first.'
+      );
     }
 
-    public static getInstance(): SecretsProvider {
-        if (!SecretsProvider.instance) {
-            SecretsProvider.instance = new SecretsProvider();
-        }
-
-        return SecretsProvider.instance;
+    if (!SecretsProvider.instance) {
+      SecretsProvider.instance = new SecretsProvider();
     }
+
+    return SecretsProvider.instance;
+  }
 }
